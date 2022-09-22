1fe1dbf09013fa71b35d7936229f415c22ece2f7
zhanglijun.9 <zhanglijun.9@bytedance.com>
2022-09-22 14:40:16 +0800

    fix mysql model

--- db/mysql/model.go
+++ db/mysql/model.go
@@ -6,7 +6,7 @@ const TableNameCounterModel = "counter_models"
 
 // CounterModel 计数器模型
 type CounterModel struct {
-	Id        int32     `gorm:"column:id" json:"id"`
+	Id        int32     `gorm:"column:id;primaryKey;autoIncrement:true" json:"id"`
 	Name      string    `gorm:"column:name" json:"name"`
 	Count     int32     `gorm:"column:count" json:"count"`
 	CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
